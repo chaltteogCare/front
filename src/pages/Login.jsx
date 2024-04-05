3130adddc984547a8cd2b19682d2d6cd3bd573e1
seojin-kim12 <rlatjwls10403@naver.com>
2024-04-05 22:43:19 +0900

    mobile-login css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,21 +11,64 @@ const Login = () => {
             exit={{ opacity: 0 }}
         >
             <Mobile>
-                <H1>hi</H1>
+                <ContainerM>
+                    <div id="title">
+                        <img src="/images/login/m_title.svg" style={{ position: "relative", top: "40px", left: "30px" }}/>
+                    </div>
+                    <div>
+                        <input style={{ position: "relative", top: "60px", left: "60px" }}
+                            name="id"
+                            type="text"
+                            placeholder="아이디"
+                        />
+                        <input style={{ position: "relative", top: "70px", left: "60px" }}
+                            name="passwd"
+                            type="text"
+                            placeholder="비밀번호"
+                        />
+                    </div>
+                    <div>
+                        <img src="/images/login/m_membership.svg" style={{ position: "relative", top: "90px", left: "170px" }}/>
+                    </div>
+                    <div>
+                        <img src="/images/login/m_btn.svg" style={{ position: "relative", top: "140px", left: "67px" }}/>
+                        <img src="/images/login/m_k_btn.svg" style={{ position: "relative", top: "155px", left: "67px" }}/>
+                    </div>
+                </ContainerM>
             </Mobile>
             <PC>
-                <H2>hello</H2>
+                <ContainerP>pc</ContainerP>
             </PC>
         </motion.div>
     );
 };
 
-const H1 = styled.div`
-    color: red;
+const ContainerM = styled.div`
+    min-height: 100vh;
+    background-color: #FFF7F0;
+
+    input {
+    width: 250px;
+    height: 31px;
+    border: none;
+    border-bottom: 1px solid black;
+    outline: none;
+    background-color: transparent;
+    color: black;
+    font-family: "HBIOS-SYS";
+    text-align: left;
+    font-size: 13px;
+  }
+
+  ::placeholder {
+    color: gray;
+    font-size: 13px;
+  }
 `;
 
-const H2 = styled.div`
-    color: blue;
+const ContainerP = styled.div`
+    min-height: 100vh;
+    background-color: #FFF7F0;
 `;
 
 export default Login;
