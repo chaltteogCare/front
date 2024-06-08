e3d306510107e5fc55798f489f5971803e785234
seojin-kim12 <rlatjwls10403@naver.com>
2024-06-08 23:09:08 +0900

    서진-setting 페이지 완성

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const Loading = React.lazy(() => import('./pages/Loading'));
 const Login = React.lazy(() => import('./pages/Login'));
 const Main = React.lazy(() => import('./pages/Main'));
 const Signup = React.lazy(() => import('./pages/Signup'));
+const Setting = React.lazy(() => import('./pages/Setting'));
 const My = React.lazy(() => import('./pages/My'));
 const Schedule = React.lazy(() => import('./pages/Schedule'));
 const Sadd = React.lazy(() => import('./pages/Sadd'));
@@ -22,6 +23,7 @@ function App() {
               <Route path="/" element={<Login />} />
               <Route path="/Main" element={<Main />} />
               <Route path="/Signup" element={<Signup />} />
+              <Route path="/Setting" element={<Setting />} />
               <Route path="/My" element={<My />} />
               <Route path="/Schedule" element={<Schedule />} /> 
               <Route path="/Sadd" element={<Sadd />} />
