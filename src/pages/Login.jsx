e20b8f96ae36e204a5f6130230cb5f9d1d41d428
seojin-kim12 <rlatjwls10403@naver.com>
2024-08-28 13:26:29 +0900

    로그인 백엔드 연동 성공

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import { Mobile, PC } from "../styles/Global_d"; 
@@ -8,59 +8,54 @@ import styled from "styled-components";
 const Login = () => {
     const navigate = useNavigate();
 
-    const onClickMain = () => {
-      navigate("/Main");
-    };
-
     const onClickSignup = () => {
         navigate("/Signup");
     };
 
     // 백엔드 연동 코드
-    const [userName, setUserName] = useState("");
+    const [name, setName] = useState("");
     const [password, setPassword] = useState("");
 
     const handleLogin = async () => {
-        if (!userName || !password) {
-          alert("아이디와 비밀번호를 입력해주세요.");
-          return; // Stop further execution
-        }
-    
-        try {
-          const response = await axios.post(
-            "http://13.124.196.200:8081/api/login",
-            {
-              password: password,
-              userName: userName,
-            }
-          );
-    
-          const isSuccess = response.data.isSuccess;
-          if (isSuccess) {
-            const memberid = response.data.result.memberId; // 아이디 받기
-            localStorage.setItem("memberid", memberid); // 아이디 저장
-            const token = response.data.result.token; // 토큰 받기
-            localStorage.setItem("token", token); // 토큰 저장
-            navigate("/Main"); // 로그인하면 홈 페이지 이동
-          } else {
-            // Handle login failure
-            const errorCode = response.data.code;
-            const errorMessage = response.data.message;
-    
-            if (errorCode === 404) {
-              alert("존재하지 않은 아이디입니다.");
-            } else if (errorCode === 401) {
-              alert("잘못된 비밀번호입니다.");
-            } else {
-              alert(`로그인 실패: ${errorMessage}`);
-            }
-          }
-        } catch (error) {
-          alert("아이디와 비밀번호를 다시 확인해주세요.");
-          console.error("로그인 실패:", error);
-        }
-      };
+      if (!name || !password) {
+        alert("아이디와 비밀번호를 입력해주세요.");
+        return; // Stop further execution
+      }
   
+      try {
+        const response = await axios.post(
+          "http://54.180.243.159:8080//api/user/login",
+          {
+            password: password,
+            name: name,
+          }
+        );
+  
+        const isSuccess = response.data.isSuccess;
+        if (isSuccess) {
+          const userId = response.data.result.userId; // 아이디 받기
+          localStorage.setItem("userId", userId); // 아이디 저장
+          const token = response.data.result.token; // 토큰 받기
+          localStorage.setItem("token", token); // 토큰 저장
+          navigate("/Main"); // 로그인 성공 시 홈 페이지로 이동
+        } else {
+          // Handle login failure
+          const errorCode = response.data.code;
+          const errorMessage = response.data.message;
+  
+          if (errorCode === 404) {
+            alert("존재하지 않은 아이디입니다.");
+          } else if (errorCode === 401) {
+            alert("잘못된 비밀번호입니다.");
+          } else {
+            alert(`로그인 실패: ${errorMessage}`);
+          }
+        }
+      } catch (error) {
+        alert("아이디와 비밀번호를 다시 확인해주세요.");
+        console.error("로그인 실패:", error);
+      }
+  };
 
     return (
         <motion.div
@@ -75,14 +70,14 @@ const Login = () => {
                     </div>
                     <div>
                         <input style={{ position: "relative", top: "60px", left: "60px" }}
-                            value={userName}
+                            value={name}
                             type="text"
-                            onChange={(e) => setUserName(e.target.value)}
+                            onChange={(e) => setName(e.target.value)}
                             placeholder="아이디"
                         />
                         <input style={{ position: "relative", top: "70px", left: "60px" }}
                             value={password}
-                            type="text"
+                            type="password" // 비밀번호 입력란 수정
                             onChange={(e) => setPassword(e.target.value)}
                             placeholder="비밀번호"
                         />
@@ -91,7 +86,7 @@ const Login = () => {
                         <img src="/images/login/m_membership.svg" style={{ position: "relative", top: "90px", left: "170px" }} onClick={onClickSignup}/>
                     </div>
                     <div>
-                        <img src="/images/login/m_btn.svg" style={{ position: "relative", top: "140px", left: "67px" }} onClick={onClickMain}/>
+                        <img src="/images/login/m_btn.svg" style={{ position: "relative", top: "140px", left: "67px" }} onClick={handleLogin}/>
                         <img src="/images/login/m_k_btn.svg" style={{ position: "relative", top: "155px", left: "67px" }}/>
                     </div>
                 </ContainerM>
